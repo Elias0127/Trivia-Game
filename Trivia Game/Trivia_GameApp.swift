c3cfb47a7d43786b63dbf3325e95855e87df1f71
Elias0127 <eliasmekonen001@gmail.com>
2024-03-29 16:51:52 -0700

    Initial Commit

---
+++ Trivia Game/Trivia_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trivia_GameApp.swift
+//  Trivia Game
+//
+//  Created by Elias Woldie on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Trivia_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
